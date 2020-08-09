e3ed388f7805d2bcb14b557d63fcea70f8dfbd88
Saida Davis <saihdavis@gmail.com>
2020-08-09 07:32:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -38,6 +38,10 @@ class Owner
     self.dogs.each  {|dog| dog.mood = "happy"}
   end
   
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+  
   def self.all
     @@all 
   end
