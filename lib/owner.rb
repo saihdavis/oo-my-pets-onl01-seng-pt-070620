ff0c5ce8d6f2a08838d871ff24f96a2ed4acf0c9
Saida Davis <saihdavis@gmail.com>
2020-08-09 07:41:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,7 +51,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs.count}dog(s), and #{self.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
   def self.all
