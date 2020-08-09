754687b05fc07b201306030bbd85880c8f6e3a47
Saida Davis <saihdavis@gmail.com>
2020-08-09 06:16:03 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,11 +3,14 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner 
     @mood = "nervous"
   end
+  
   
   
    
